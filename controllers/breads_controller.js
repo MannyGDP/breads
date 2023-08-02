1f099308ee7df5d5a7c03b7d785a47b789a39087
Manny Delgado <mannygdp@gmail.com>
2023-08-01 23:50:55 -0700

    change

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -12,6 +12,11 @@ breads.get ('/', (req, res) => {
  );
 });
 
+// NEW
+breads.get('/new', (req, res) => {
+    res.render('New');
+});
+
 // Show
 breads.get('/:arrayIndex', (req, res) => {
     if (Bread[req.params.arrayIndex]) {
@@ -21,6 +26,17 @@ breads.get('/:arrayIndex', (req, res) => {
     } else {
         res.render('Error404');
     }
+});
+
+// CREATE
+breads.post('/', (req, res) => {
+    if (req.body.hasGluten === 'on') {
+        req.body.hasGluten = 'true';
+    } else {
+        req.body.hasGluten = 'false';
+    }
+    Bread.push(req.body);
+    res.redirect('/breads');
 });
 
 module.exports = breads;
