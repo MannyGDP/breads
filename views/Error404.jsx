c9ce764d831a25b96c7c9142588b5e7817c14da7
Manny Delgado <mannygdp@gmail.com>
2023-08-01 22:53:16 -0700

    done

--- views/Error404.jsx
+++ views/Error404.jsx
@@ -1,7 +1,7 @@
 const React = require('react');
 const Default = require('./layouts/Default');
 
-function Error404(){
+function Error404() {
     return (
         <Default>
             <h1>404: Bread doesn't exist</h1>
