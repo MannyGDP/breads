33dc2441cce46008becae21750765a549c013c71
Manny Delgado <mannygdp@gmail.com>
2025-02-06 22:38:36 -0800

    "made changes"

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 //Dependencies
 const express = require ('express');
 const methodOverride = require('method-override');
+const mongoose = require('mongoose');
 require('dotenv').config();
-// const mongoose = require('mongoose');
+
 const { PORT } = process.env;
 
 // Configuration
@@ -16,6 +17,8 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true}));
 app.use(methodOverride('_method'));
 
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
 // Routes
 app.get('/', (req, res) => {
     res.send('Welcome to an Asesome App about Breads!');
@@ -23,6 +26,7 @@ app.get('/', (req, res) => {
 
 // Breads start
 const breadsController = require('./controllers/breads_controller');
+// const { default: mongoose } = require('mongoose');
     app.use('/breads', breadsController);
 
 // 404 Page
